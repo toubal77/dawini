5f12acc6c19ebdfa65b98e61036df247cf02c896
toubal77 <72252709+toubal77@users.noreply.github.com>
2022-06-03 14:27:06 +0200

    add powerdBy in home page

--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -1,13 +1,16 @@
 import 'package:dawini/app/new_patient/new_patient_screen.dart';
 import 'package:dawini/common_widgets/custom_elevated_button.dart';
+import 'package:dawini/common_widgets/size_config.dart';
 import 'package:dawini/constants/app_images.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    SizeConfig.init(context);
     return Scaffold(
       body: Container(
         decoration: const BoxDecoration(
@@ -16,66 +19,102 @@ class HomePage extends StatelessWidget {
             fit: BoxFit.fitHeight,
           ),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Stack(
           children: [
-            CustomElevatedButton(
-              buttonText: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Padding(
-                    padding: EdgeInsets.only(left: 20.0),
-                    child: Text(
-                      "Ajoute un patient",
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.white,
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CustomElevatedButton(
+                  buttonText: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Padding(
+                        padding: EdgeInsets.only(left: 20.0),
+                        child: Text(
+                          "Ajoute un patient",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                          ),
+                        ),
                       ),
-                    ),
+                      Icon(
+                        Icons.chevron_right,
+                        size: 30,
+                      ),
+                    ],
                   ),
-                  Icon(
-                    Icons.chevron_right,
-                    size: 30,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        fullscreenDialog: true,
+                        builder: (context) {
+                          return const NewPatientScreen();
+                        },
+                      ),
+                    );
+                  },
+                  minHeight: 35,
+                  minWidth: 200,
+                ),
+                CustomElevatedButton(
+                  buttonText: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Padding(
+                        padding: EdgeInsets.only(left: 20.0),
+                        child: Text(
+                          "Voir mes patients",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      Icon(
+                        Icons.chevron_right,
+                        size: 30,
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    fullscreenDialog: true,
-                    builder: (context) {
-                      return const NewPatientScreen();
-                    },
-                  ),
-                );
-              },
-              minHeight: 35,
-              minWidth: 200,
+                  onPressed: () {},
+                  minHeight: 35,
+                  minWidth: 200,
+                ),
+              ],
             ),
-            CustomElevatedButton(
-              buttonText: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Padding(
-                    padding: EdgeInsets.only(left: 20.0),
-                    child: Text(
-                      "Voir mes patients",
+            Positioned(
+              bottom: 15,
+              right: SizeConfig.screenHeight * 0.15,
+              left: SizeConfig.screenHeight * 0.15,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 30.h,
+                    child: const Text(
+                      'powerdBy',
                       style: TextStyle(
+                        color: Color.fromARGB(255, 216, 91, 91),
                         fontSize: 16,
-                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
                       ),
                     ),
                   ),
-                  Icon(
-                    Icons.chevron_right,
-                    size: 30,
+                  SizedBox(
+                    height: 30.h,
+                    child: const Text(
+                      'Dr Toubal M.H.M',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 216, 91, 91),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
                   ),
                 ],
               ),
-              onPressed: () {},
-              minHeight: 35,
-              minWidth: 200,
             ),
           ],
         ),
