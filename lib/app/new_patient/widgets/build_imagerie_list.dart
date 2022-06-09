acee15b74f050d1abde63fd73334df2a49937d80
toubal77 <72252709+toubal77@users.noreply.github.com>
2022-06-09 18:35:04 +0200

    make remove imagerie list

--- lib/app/new_patient/widgets/build_imagerie_list.dart
+++ lib/app/new_patient/widgets/build_imagerie_list.dart
@@ -2,18 +2,23 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class BuildImagerieList extends StatelessWidget {
+class BuildImagerieList extends StatefulWidget {
   final List<dynamic>? imagerie;
   const BuildImagerieList({Key? key, required this.imagerie}) : super(key: key);
 
   @override
+  State<BuildImagerieList> createState() => _BuildImagerieListState();
+}
+
+class _BuildImagerieListState extends State<BuildImagerieList> {
+  @override
   Widget build(BuildContext context) {
     return ListView.builder(
       shrinkWrap: true,
-      itemCount: imagerie!.length,
+      itemCount: widget.imagerie!.length,
       itemBuilder: (context, index) {
         bool isDate = false;
-        switch (imagerie![index]['imagerieDate'].runtimeType) {
+        switch (widget.imagerie![index]['imagerieDate'].runtimeType) {
           case DateTime:
             {
               isDate = true;
@@ -28,53 +33,72 @@ class BuildImagerieList extends StatelessWidget {
         return Padding(
           padding: const EdgeInsets.all(8.0),
           child: IntrinsicHeight(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            child: Row(
               children: [
-                Flexible(
-                  child: RichText(
-                    text: TextSpan(
-                      text:
-                          '-typeImagerie: ${imagerie![index]['typeImagerie']} ',
-                      style: DefaultTextStyle.of(context).style,
-                    ),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-                Flexible(
-                  child: RichText(
-                    text: TextSpan(
-                      text: isDate
-                          ? '-imagerieDate: ${DateFormat('dd-MMM-yyy').format(DateTime.parse(imagerie![index]['imagerieDate'].toString()))} '
-                          : '-imagerieDate: ${DateFormat('dd-MMM-yyy').format(DateTime.parse(imagerie![index]['imagerieDate'].toDate().toString()))} ',
-                      style: DefaultTextStyle.of(context).style,
-                    ),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-                Flexible(
-                  child: RichText(
-                    text: TextSpan(
-                      text: '-Conclusion: ',
-                      style: DefaultTextStyle.of(context).style,
-                    ),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-                for (int i = 0; i < imagerie![index]['imagerie'].length; i++)
-                  Flexible(
-                    child: RichText(
-                      text: TextSpan(
-                        text: '     -${imagerie![index]['imagerie'][i]} ',
-                        style: DefaultTextStyle.of(context).style,
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Flexible(
+                      child: RichText(
+                        text: TextSpan(
+                          text:
+                              '-typeImagerie: ${widget.imagerie![index]['typeImagerie']} ',
+                          style: DefaultTextStyle.of(context).style,
+                        ),
+                        overflow: TextOverflow.ellipsis,
                       ),
-                      overflow: TextOverflow.ellipsis,
                     ),
+                    Flexible(
+                      child: RichText(
+                        text: TextSpan(
+                          text: isDate
+                              ? '-imagerieDate: ${DateFormat('dd-MMM-yyy').format(DateTime.parse(widget.imagerie![index]['imagerieDate'].toString()))} '
+                              : '-imagerieDate: ${DateFormat('dd-MMM-yyy').format(DateTime.parse(widget.imagerie![index]['imagerieDate'].toDate().toString()))} ',
+                          style: DefaultTextStyle.of(context).style,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    Flexible(
+                      child: RichText(
+                        text: TextSpan(
+                          text: '-Conclusion: ',
+                          style: DefaultTextStyle.of(context).style,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    for (int i = 0;
+                        i < widget.imagerie![index]['imagerie'].length;
+                        i++)
+                      Flexible(
+                        child: RichText(
+                          text: TextSpan(
+                            text:
+                                '     -${widget.imagerie![index]['imagerie'][i]} ',
+                            style: DefaultTextStyle.of(context).style,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    const SizedBox(height: 5),
+                    const Divider(
+                      height: 2,
+                      color: Colors.grey,
+                    ),
+                  ],
+                ),
+                IconButton(
+                  onPressed: () {
+                    setState(() {
+                      widget.imagerie!.removeWhere(
+                          (element) => element == widget.imagerie![index]);
+                    });
+                  },
+                  icon: const Icon(
+                    Icons.delete,
+                    color: Colors.red,
                   ),
-                const SizedBox(height: 5),
-                const Divider(
-                  height: 2,
-                  color: Colors.grey,
                 ),
               ],
             ),
